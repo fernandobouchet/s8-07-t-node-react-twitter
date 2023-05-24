81df66d38e8f84466f3decb355a4323a52b78bcf
Maximiliano <maximartins45@gmail.com>
2023-05-24 02:10:35 -0300

    agregue redireccion en el home

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -5,53 +5,64 @@ import Link from "next/link";
 import { initialState } from "./../../../data/tweets";
 import Head from "next/head";
 import { getAllTweets } from "../../../lib/tweets";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+
 function Home() {
   const [isSelected, setIsSelected] = useState("para-ti");
   const [allTweets, setAllTweets] = useState(initialState);
   const [auxAllTweets, setAuxAllTweets] = useState(initialState);
 
-    const filterTweets = (payload) => {
-        setAllTweets(auxAllTweets.filter(tweet => payload === "siguiendo" ? tweet.author.username !== 'Cristiano' : tweet.author.username !== ''))
-    }
-    const addTweets = (payload) => {
-        setAuxAllTweets([payload, ...auxAllTweets])
-        setAllTweets([payload, ...auxAllTweets])
-    }
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const data = await getAllTweets();
-          setAllTweets(prevState => (
-            [...data, ...prevState]
-          ));
-        } catch (error) {
-          console.log(error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-    useEffect(() => {
-        filterTweets(isSelected)
-    }, [isSelected])
+  const filterTweets = (payload) => {
+    setAllTweets(auxAllTweets.filter(tweet => payload === "siguiendo" ? tweet.author.username !== 'Cristiano' : tweet.author.username !== ''))
+  }
+  const addTweets = (payload) => {
+    setAuxAllTweets([payload, ...auxAllTweets])
+    setAllTweets([payload, ...auxAllTweets])
+  }
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllTweets();
+        setAllTweets(prevState => (
+          [...data, ...prevState]
+        ));
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    return (
-        <>
-            <Head>
-                <title>Inicio / Twitter</title>
-            </Head>
-            <Header isSelected={isSelected} setIsSelected={setIsSelected} />
-            <Post addTweets={setAllTweets} />
-            {
-                allTweets.length && allTweets.map((tweet) => (
-                    <Tweet key={tweet._id} {...tweet} />
-                ))
-            }
-        </>
-    )
+    fetchData();
+  }, []);
+  useEffect(() => {
+    filterTweets(isSelected)
+  }, [isSelected])
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+    return <></>
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Inicio / Twitter</title>
+      </Head>
+
+      <Header isSelected={isSelected} setIsSelected={setIsSelected} />
+      <Post addTweets={setAllTweets} />
+      {
+    allTweets.length && allTweets.map((tweet) => (
+      <Tweet key={tweet._id} {...tweet} />
+    ))
+  }
+    </>
+  )
 }
 
 export default Home;
@@ -69,36 +80,32 @@ const Header = ({ isSelected, setIsSelected }) => {
       <div className="flex text-[#536471]">
         <div
           onClick={() => setIsSelected("para-ti")}
-          className={`flex items-center justify-center w-full h-[60px] text-md p-4 ${
-            isSelected === "para-ti"
-              ? "font-bold text-black dark:text-[#e7e9ea]"
-              : "text-gray-500 dark:text-[#536471] font-semibold"
-          }  hover:bg-gray-500 hover:bg-opacity-10 cursor-pointer transition duration-200 ease-in-out`}
+          className={`flex items-center justify-center w-full h-[60px] text-md p-4 ${isSelected === "para-ti"
+            ? "font-bold text-black dark:text-[#e7e9ea]"
+            : "text-gray-500 dark:text-[#536471] font-semibold"
+            }  hover:bg-gray-500 hover:bg-opacity-10 cursor-pointer transition duration-200 ease-in-out`}
         >
           <div
-            className={`inline-block text-center border-b-4 ${
-              isSelected === "para-ti"
-                ? "border-b-[#1C9CEF]"
-                : "border-b-transparent"
-            } h-[60px] `}
+            className={`inline-block text-center border-b-4 ${isSelected === "para-ti"
+              ? "border-b-[#1C9CEF]"
+              : "border-b-transparent"
+              } h-[60px] `}
           >
             <div className="my-auto mt-4">Para ti </div>
           </div>
         </div>
         <div
           onClick={() => setIsSelected("siguiendo")}
-          className={`flex items-center justify-center w-full h-[60px] text-md  p-4 ${
-            isSelected === "siguiendo"
-              ? "font-bold text-black dark:text-[#e7e9ea]"
-              : "text-gray-500 dark:text-[#536471] font-semibold"
-          }  hover:bg-gray-500 hover:bg-opacity-10 cursor-pointer transition duration-200 ease-in-out`}
+          className={`flex items-center justify-center w-full h-[60px] text-md  p-4 ${isSelected === "siguiendo"
+            ? "font-bold text-black dark:text-[#e7e9ea]"
+            : "text-gray-500 dark:text-[#536471] font-semibold"
+            }  hover:bg-gray-500 hover:bg-opacity-10 cursor-pointer transition duration-200 ease-in-out`}
         >
           <div
-            className={`inline-block text-center border-b-4 ${
-              isSelected === "siguiendo"
-                ? "border-b-[#1C9CEF]"
-                : "border-b-transparent"
-            } h-[60px] `}
+            className={`inline-block text-center border-b-4 ${isSelected === "siguiendo"
+              ? "border-b-[#1C9CEF]"
+              : "border-b-transparent"
+              } h-[60px] `}
           >
             <div className="my-auto mt-4">Siguiendo </div>
           </div>
