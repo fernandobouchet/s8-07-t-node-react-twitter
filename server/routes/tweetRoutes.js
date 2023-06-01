cfb6939bed8c7243d3d61940b810a6f3e100b401
martaalcazarr <martaalcazarredondo@gmail.com>
2023-05-31 21:15:22 -0400

    add: tweets by date, top tweets, top hashtags

--- server/routes/tweetRoutes.js
+++ server/routes/tweetRoutes.js
@@ -9,7 +9,10 @@ import {
   updateTweet,
   likeTweet,
   getAllTweets,
-  getAllFollowsTweets
+  getAllFollowsTweets,
+  getTweetsByDate,
+  getTopTweets,
+  getTopHashtags
 } from '../controllers/tweetController.js';
 import upload from '../middlewares/multer.js';
 import { sessionMiddleware } from '../middlewares/sessionMiddleware.js';
@@ -17,6 +20,9 @@ import { sessionMiddleware } from '../middlewares/sessionMiddleware.js';
 const tweetRouter = Router();
 
 tweetRouter.get('/all', getAllTweets);
+tweetRouter.get('/tweetsByDate', getTweetsByDate);
+tweetRouter.get('/topTweets', getTopTweets);
+tweetRouter.get('/topHashtags', getTopHashtags)
 tweetRouter.get('/allFollowed', sessionMiddleware, getAllFollowsTweets)
 
 tweetRouter.get('/user/:userId', getTweetsByUserId);
@@ -30,5 +36,6 @@ tweetRouter.post('/retweet/:id', sessionMiddleware, createRetweet);
 tweetRouter.delete('/retweet/:id', sessionMiddleware, deleteRetweet);
 
 tweetRouter.put('/like/:id', sessionMiddleware, likeTweet);
+
 
 export default tweetRouter;
